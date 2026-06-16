d0e649f494c1e9e8ccb5028188d81654416d8dd4
Jesus Lechuga <jesusleg95@outlook.com>
2026-06-16 15:39:41 -0600

    Dashboard Administracion

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\LogoutController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,9 @@ Route::post('/auth/register', [RegisterController::class, 'store'])->name('regis
 // Pantalla de iniciar sesion
 Route::get('/auth/login', [LoginController::class, 'index'])->name('login');
 Route::post('/auth/login', [LoginController::class, 'store'])->name('login.store');
+
+// Cerrar Sesion
+Route::post('/auth/logout', [LogoutController::class, 'store'])->name('logout.store');
 
 // Verificacion
 Route::get('/email/verify/{id}/{hash}', function(EmailVerificationRequest $request){
